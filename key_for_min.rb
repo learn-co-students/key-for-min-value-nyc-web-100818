5613f68653cb90216b5813f846c0d6ead90b5b65
github username <github email address>
2018-09-19 14:51:42 -0400

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,14 @@
 
 def key_for_min_value(name_hash)
 
+  min = nil
+  key = nil
+
+  name_hash.each do |name, n|
+    if min == nil || n < min
+       min = n
+       key = name
+    end
+  end
+  key
 end
