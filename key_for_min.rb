99fc3414054fc0cf5a6cc2d73b7e9eb270042a63
Henry <hanke.liu@gmail.com>
2018-10-03 17:47:55 -0400

    passed but try again

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,16 @@
+#require 'pry'
 # prereqs: iterators, hashes, conditional logic
+
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+
+
+  sorted_array = name_hash.sort_by {|k, v| v}
+
+  sorted_hash = sorted_array.to_h
+  sorted_hash.keys.first
+
+#binding.pry
 
 end
